9b27b0eb4eac1bc5fa095b7a092c6b68d3a4dbbe
YasarDogann <yasardgn99@gmail.com>
2024-10-19 10:27:44 +0300

    polymorphism

---
+++ patika_w4_Polymorphism/BaseGeometricShape.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patika_w4_Polymorphism
+{
+    // Her bir geometrik şeklin ortak özelliklerini Yani kenar ve yüksekliklerini properties olarak alıyoruz abstract class olarak tanımla.
+    public abstract class BaseGeometricShape
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+
+        // virtual bir class olarak tanımladım diğer classlarda bunu override ederek tekrardan kullanabilir ve değişiklik yapabiliriz.
+        public virtual void CalculateArea()
+        {
+            Console.WriteLine("Geometrik Şeklin Alanı");
+        }
+    }
+
+    // Kare Sınıfı oluşturdum. BaseGeometricShape sınıfından base sınıftan miras aldı.
+    public class Square : BaseGeometricShape
+    {
+        // Burada bir yapıcı method tanımladım. Özelliklerini base classtan çekiyoruz ve parametre giriyoruz
+        public Square(double heightt, double width)
+        {
+            Height = heightt;
+            Width = width;
+        }
+        public override void CalculateArea()  // virtual olarak base class'da tanımlanan methodu override ile tekrardan yazdık ve kare özelliklerine göre doldurdum.
+        {
+            double area = Height * Width;
+            Console.WriteLine($"Karenin Alanı: {area}");
+        }
+    }
+
+    public class Rentagle : BaseGeometricShape
+    {
+        public Rentagle(double height, double width)
+        {
+            Height= height;
+            Width = width;
+        }
+        public override void CalculateArea()// virtual olarak base class'da tanımlanan methodu override ile tekrardan yazdık ve dikdörtgen özelliklerine göre doldurdum.
+        {
+            double area = Height * Width;
+            Console.WriteLine($"Dikdörtgenin Alanı : {area}");
+        }
+    }
+
+    public class RightTriangle : BaseGeometricShape
+    {
+        public RightTriangle(double height, double width)
+        {
+            Height= height;
+            Width= width;
+        }
+        public override void CalculateArea()// virtual olarak base class'da tanımlanan methodu override ile tekrardan yazdık ve dik üçgen özelliklerine göre doldurdum.
+        {
+            double area = (Height * Width) / 2;
+            Console.WriteLine($"Dik Üçgenin Alanı : {area}");
+        }
+    }
+}
---
+++ patika_w4_Polymorphism/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace patika_w4_Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Polymorphism ile nesne oluşturdum
+            BaseGeometricShape square = new Square(15,6);
+            square.CalculateArea();
+
+            Console.WriteLine("\r\n***************************************\r\n");
+
+            BaseGeometricShape rentagle = new Rentagle(20,15.3);
+            rentagle.CalculateArea();
+
+            Console.WriteLine("\r\n***************************************\r\n");
+
+            BaseGeometricShape rightTriangle = new RightTriangle(20, 6);
+            rightTriangle.CalculateArea();
+        }
+    }
+}
